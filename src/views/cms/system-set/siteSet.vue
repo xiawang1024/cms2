dfb7f56fbe94cdd1d2a8af742fe57ccc6aebd97d
alexgaoyh <alexgaoyh@sina.com>
2019-03-04 09:01:52 +0800

    站点设置增加动态新增修改功能.

--- src/views/cms/system-set/siteSet.vue
+++ src/views/cms/system-set/siteSet.vue
@@ -1,20 +1,64 @@
 <template>
   <div class="site-set-container">
+    <div class="tool-bar">
+      <el-button type="primary" @click="handleAddDialog()">新增</el-button>
+    </div>
     <el-table :data="dictObj.details" style="width: 100%">
       <el-table-column prop="dictDetailName" label="名称"/>
-      <el-table-column prop="dictDetailCode" label="代码"/>
+      <el-table-column label="代码">
+        <template slot-scope="scope">
+          <el-input v-model="scope.row.dictDetailCode"/>
+        </template>
+      </el-table-column>
       <el-table-column label="值">
         <template slot-scope="scope">
           <el-input v-model="scope.row.dictDetailValue"/>
         </template>
       </el-table-column>
     </el-table>
+
+    <el-dialog :visible.sync="addSiteSetVisible" title="添加站点设置">
+      <el-form :model="siteSetForm">
+        <el-form-item label="设置名称">
+          <el-input v-model="siteSetForm.dictDetailName"/>
+        </el-form-item>
+        <el-form-item label="代码">
+          <el-input v-model="siteSetForm.dictDetailCode"/>
+        </el-form-item>
+        <el-form-item label="值">
+          <el-input v-model="siteSetForm.dictDetailValue"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addSiteSetVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleAdd()">确 定</el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog :visible.sync="alterSiteSetVisible" title="修改站点设置">
+      <el-form :model="siteSetForm">
+        <el-form-item label="设置名称">
+          <el-input :disabled="true" v-model="siteSetForm.dictDetailName"/>
+        </el-form-item>
+        <el-form-item label="代码">
+          <el-input v-model="siteSetForm.dictDetailCode"/>
+        </el-form-item>
+        <el-form-item label="设置路径">
+          <el-input v-model="siteSetForm.dictDetailValue"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="alterSiteSetVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleAlter()">确 定</el-button>
+      </div>
+    </el-dialog>
+
     <el-button type="primary" class="btn-save" @click="handleSubmit()">保存</el-button>
   </div>
 </template>
 
 <script>
-import { fetchDictByDictName, updateDict } from '@/api/cms/dict'
+import { fetchDictByDictName, createDict, updateDict } from '@/api/cms/dict'
 export default {
   name: 'SiteSet',
   data() {
@@ -35,6 +79,13 @@ export default {
         clientLicenseId: '',
         details: [
         ]
+      },
+      addSiteSetVisible: false,
+      alterSiteSetVisible: false,
+      siteSetForm: {
+        dictDetailId: '',
+        dictDetailName: '',
+        dictDetailValue: ''
       }
     }
   },
@@ -42,6 +93,54 @@ export default {
     this.fetchDict()
   },
   methods: {
+    handleAddDialog() {
+      this.siteSetForm.dictDetailId = ''
+      this.siteSetForm.dictDetailName = ''
+      this.siteSetForm.dictDetailValue = ''
+      this.addSiteSetVisible = true
+    },
+    handleAdd() {
+      console.log('新增')
+      this.handleDialogObjToList()
+      this.addSiteSetVisible = false
+      // 新增保存
+      this.handleSubmit()
+    },
+    beforeAlter(index, row) {
+      this.siteSetForm.dictDetailId = row.dictDetailId
+      this.siteSetForm.dictDetailName = row.dictDetailName
+      this.siteSetForm.dictDetailValue = row.dictDetailValue
+      this.alterSiteSetVisible = true
+    },
+    handleAlter() {
+      console.log('修改')
+      this.handleDialogObjToList()
+      this.alterSiteSetVisible = false
+      // 修改保存
+      this.handleSubmit()
+    },
+    handleDelete(index, row) {
+      console.log('删除' + row.name)
+      this.dictObj.details.splice(index, 1)
+      // 删除保存
+      this.handleSubmit()
+    },
+    // 将当前弹出框的数据重新维护到 dictObj.details 中
+    handleDialogObjToList() {
+      var _this = this
+      var currentOpeIdx = -1
+      _this.dictObj.details.forEach(function(v, k) {
+        if (v.dictDetailName === _this.siteSetForm.dictDetailName) {
+          currentOpeIdx = k
+        }
+      })
+      if (currentOpeIdx === -1) {
+        _this.dictObj.details.push(Object.assign({}, _this.siteSetForm))
+      } else {
+        _this.dictObj.details[currentOpeIdx].dictDetailName = _this.siteSetForm.dictDetailName
+        _this.dictObj.details[currentOpeIdx].dictDetailValue = _this.siteSetForm.dictDetailValue
+      }
+    },
     // 查询对象
     fetchDict() {
       var _this = this
@@ -62,27 +161,46 @@ export default {
     // 保存对象，实际为更新
     handleSubmit() {
       var _this = this
-      return new Promise((resolve, reject) => {
-        updateDict(_this.dictObj)
-          .then((response) => {
-            _this.fetchDict()
-            resolve()
-          })
-          .catch((error) => {
-            reject(error)
-          })
-      })
+      if (_this.dictObj.dictId === '' || _this.dictObj.dictId === null) {
+        _this.dictObj.dictName = '站点设置'
+        return new Promise((resolve, reject) => {
+          createDict(_this.dictObj)
+            .then((response) => {
+              _this.fetchDict()
+              resolve()
+            })
+            .catch((error) => {
+              reject(error)
+            })
+        })
+      } else {
+        return new Promise((resolve, reject) => {
+          updateDict(_this.dictObj)
+            .then((response) => {
+              _this.fetchDict()
+              resolve()
+            })
+            .catch((error) => {
+              reject(error)
+            })
+        })
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-.site-set-container {
-  margin: 30px;
-}
+  .el-button + .el-button {
+    margin-top: 5px;
+    margin-left: 0px;
+  }
 
-.btn-save {
-  margin-top: 20px;
-}
+  .siteSet-container {
+    margin: 30px;
+  }
+
+  .tool-bar {
+    text-align: right;
+  }
 </style>
