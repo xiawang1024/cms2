87893f5d5d966266b0a1960450341a9f0dc76830
zhaojinpeng <1406737142@qq.com>
2019-03-26 16:38:23 +0800

    栏目修改

--- src/components/cms/WebComponents/components/docs/mockData.js
+++ src/components/cms/WebComponents/components/docs/mockData.js
@@ -15,25 +15,25 @@ export const currentDoc = [
     id: '4',
     label: '删除'
   },
-  // {
-  //   id: '5',
-  //   label: '复制到'
-  // },
-  // {
-  //   id: '6',
-  //   label: '移动到'
-  // },
-  // {
-  //   id: '7',
-  //   label: '引用到'
-  // }
+  {
+    id: '5',
+    label: '复制到'
+  },
+  {
+    id: '6',
+    label: '移动到'
+  },
+  {
+    id: '7',
+    label: '引用到'
+  }
 ]
 
 export const currentColumn = [
-  // {
-  //   id: '1',
-  //   label: '发布栏目首页'
-  // },
+  {
+    id: '1',
+    label: '发布栏目首页'
+  },
   // {
   //   id: '2',
   //   label: '指定范围重新发布'
@@ -42,34 +42,34 @@ export const currentColumn = [
     id: '3',
     label: '修改栏目'
   },
-  // {
-  //   id: '4',
-  //   label: '栏目图片'
-  // },
-  // {
-  //   id: '5',
-  //   label: '自定义文档列表'
-  // },
-  // {
-  //   id: '6',
-  //   label: '自定义内容片断'
-  // },
-  // {
-  //   id: '7',
-  //   label: '模板'
-  // },
-  // {
-  //   id: '8',
-  //   label: '管理评论'
-  // },
-  // {
-  //   id: '9',
-  //   label: '废件箱'
-  // },
-  // {
-  //   id: '10',
-  //   label: '发稿和浏览统计'
-  // }
+  {
+    id: '4',
+    label: '栏目图片'
+  },
+  {
+    id: '5',
+    label: '自定义文档列表'
+  },
+  {
+    id: '6',
+    label: '自定义内容片断'
+  },
+  {
+    id: '7',
+    label: '模板'
+  },
+  {
+    id: '8',
+    label: '管理评论'
+  },
+  {
+    id: '9',
+    label: '废件箱'
+  },
+  {
+    id: '10',
+    label: '发稿和浏览统计'
+  }
 ]
 
 export const fromOptions = [
