edb576bab6976e1241c1ca9692359cf6ee2ca6e5
zhaojinpeng <1406737142@qq.com>
2019-03-27 14:36:59 +0800

    文档列表

--- src/components/cms/WebComponents/components/customDocList/choosedList.vue
+++ src/components/cms/WebComponents/components/customDocList/choosedList.vue
@@ -1,26 +1,48 @@
 <template>
-  <el-table
-    :data="tableData"
-    style="width: 100%">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180"/>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180"/>
-    <el-table-column
-      prop="address"
-      label="地址"/>
-      <!-- <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column> -->
-  </el-table>
+  <div class="document-dialog">
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="标题"
+        width="120"/>
+      <el-table-column
+        prop="name"
+        label="类型"
+        width="120"/>
+      <el-table-column
+        prop="name"
+        label="所属栏目"/>
+      <el-table-column
+        prop="name"
+        label="标记"/>
+      <el-table-column
+        prop="name"
+        label="发稿人"/>
+      <el-table-column
+        prop="name"
+        label="发布时间"/>
+      <el-table-column
+        prop="name"
+        label="点击"/>
+      <el-table-column
+        prop=""
+        label="操作">
+        <template slot="header" slot-scope="scope">
+          <el-button type="primary" size="small" @click="chooseDoc">添加</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <document-dialog :dialog-visible.sync = "dialogVisible"/>
+  </div>
 </template>
 <script>
+  import documentDialog from './documentDialog.vue'
   export default {
+    components: {
+      documentDialog
+    },
     data() {
       return {
         tableData: [{
@@ -39,7 +61,13 @@
           date: '2016-05-03',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1516 弄'
-        }]
+        }],
+        dialogVisible: false
+      }
+    },
+    methods: {
+      chooseDoc() {
+        this.dialogVisible = true
       }
     }
   }
---
+++ src/components/cms/WebComponents/components/customDocList/documentDialog.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="choose-handel-dialog">
+    <el-dialog
+      :visible.sync="dialogVisible"
+      title="选择文章"
+      width="60%"
+      :before-close="colseMe"
+      :append-to-body = "true"
+    >
+      <!-- <span slot="footer" class="dialog-footer">
+        <el-button @click="$emit('update:dialogVisible', false)" size="small">取消</el-button>
+        <el-button type="primary" @click="$emit('update:dialogVisible', false)" size="small">确定</el-button>
+      </span> -->
+      <div>
+        <split-pane
+          :min-percent="10"
+          :max-percent="30"
+          :default-percent="12"
+          class="pane-wrap"
+          split="vertical"
+        >
+          <template slot="paneL">
+            <div class="left-container">
+              <el-scrollbar
+                wrap-class="scrollbar-wrapper"
+                style="height:100%;"
+              > 
+                <div>rkwrjjwer9rr</div>
+                <!-- <tree :tree-data = "tableData"/> -->
+              </el-scrollbar>
+            </div>
+          </template>
+          <template slot="paneR">
+            <div class="right-container">
+              <el-scrollbar
+                wrap-class="scrollbar-wrapper"
+                style="height:100%;"
+              > 
+                <div>wjrjw9jtjttjetttj</div>
+                <!-- <WebComponents :component-type="contextMenu.id" /> -->
+              </el-scrollbar>
+            </div>
+          </template>
+        </split-pane>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+<script>
+import splitPane from 'vue-splitpane'
+export default {
+  name: '',
+  components: {
+    splitPane
+  },
+  props: {
+    dialogVisible: {
+      default: false,
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      tableData: [],
+      chooseIds: []
+    }
+  },
+  watch: {
+    dialogVisible(val) {
+      // if(val) {
+      //   this.chooseIds = []
+      //   this.tableData = this.multipleList
+      //   this.tableData.forEach((ele) => {
+      //     ele.sendTime = Date.now()
+      //     ele.handelTime =  Date.now()
+      //     ele.modifyTime =  ''
+      //     this.chooseIds.push(ele.articleId)
+      //   })
+      // }
+    }
+  },
+  methods: {
+    colseMe() {
+      console.log(111)
+      this.$emit('update:dialogVisible', false)
+    }
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+.choose-handel-dialog {
+  position: relative;
+  height: calc(100vh - 124px);
+  margin: 12px 10px;
+}
+.pane-wrap {
+  margin-top: 12px;
+}
+.left-container {
+  /* background-color: #f38181; */
+  // height: 100%;
+  /* overflow: hidden; */
+}
+
+.right-container {
+  // background-color: #fce38a;
+  // height: 100%;
+}
+</style>
+
