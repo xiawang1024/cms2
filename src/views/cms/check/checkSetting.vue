737273ea918cbe7f43eb302717b5c43d50391455
zhaojinpeng <1406737142@qq.com>
2019-04-12 10:28:40 +0800

    审核设置

--- src/views/cms/check/checkSetting.vue
+++ src/views/cms/check/checkSetting.vue
@@ -1,22 +1,274 @@
 <template>
   <div class="check-setting">
-    审核管理
+    <div class="el-card__header">
+      <v-search :search-settings="searchSettings" @search="searchItem"/>
+    </div>
+    <div class="tool-bar">
+      <el-button type="primary" size="small" @click="addCheck">添加</el-button>
+    </div>
+    <el-table :data="tableData" style="width: 100%" highlight-current-row size="mini">
+      <el-table-column prop="channelName" label="配置名称" min-width="250" show-overflow-tooltip>
+        <template slot-scope="scope">
+          <!-- <div>
+            <span v-for= "(group) in channelNameChange(scope.row.parentChannelNames)" :key="group" class="space-length"/>
+            <span v-for= "(ele, index) in channelNameChange(scope.row.parentChannelNames)" :key="index" class="space-holder"/>
+            <span>{{ scope.row.parentChannelNames }}</span>
+            <span>{{ scope.row.channelName }}</span>
+          </div> -->
+        </template>
+      </el-table-column>
+      <el-table-column prop="channelCode" label="排序" min-width="80"/>
+      <el-table-column prop="hiddenFlag" label="是否启用">
+        <template slot-scope="scope">
+          <span v-if="scope.row.hiddenFlag == 1">隐藏</span>
+          <span v-else>显示</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="parentChannelId" label="添加时间" width="100">
+        <template slot-scope="scope">
+          <span v-if="scope.row.parentChannelId == '-1'">是</span>
+          <span v-else>否</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="150">
+        <template slot-scope="scope">
+          <el-button type="text" @click="editCheck(scope.rwo)">编辑</el-button>
+          <el-button type="text" @click="columnDel(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <pagination :total="totalCount"/>
+    <v-page :visible.sync="handelCheck" @goBack="goBack">
+      <h3 slot="title">{{ title }}</h3>
+      <template slot="content">
+        <!-- 详情页组件 -->
+        <!-- <v-form ref="vform" :form-settings="formSettings" :form-data="formData" @save="submitSave">
+          <template slot="list">
+            <div class="choosed-list">
+              <choosed-list ref="choosedList" :details-list = "detailsList"/>
+            </div>
+          </template>
+        </v-form> -->
+      </template>
+    </v-page>
   </div>
 </template>
+
 <script>
-import qs from 'qs' // 序列化表单数据
+// import { fetchDictByDictName } from '@/api/cms/dict'
+import { columnList, deleteColumn } from '@/api/cms/columnManage'
+import mixins from '@/components/cms/mixins'
+import Pagination from '@/common/Pagination'
 export default {
-  name: 'CheckSetting',
+  name: 'ColumnManage',
+  components: {
+    Pagination
+  },
+  mixins: [mixins],
   data() {
-    return {}
+    return {
+      tableData: [],
+      pageNum: 1,
+      pageSize: 10,
+      totalCount: 0,
+      searchSettings: [{
+        label: '栏目名称',
+        name: 'channelName',
+        placeholder: '请输入栏目名称',
+        visible: true,
+        options: [],
+        type: 'cascader'
+      }, {
+        label: '状态',
+        name: 'hiddenFlag',
+        placeholder: '请选择',
+        visible: true,
+        type: 'select',
+        options: [
+          {
+            label: '显示',
+            value: 0          
+          },
+          {
+            label: '隐藏',
+            value: 1          
+          }
+        ]
+      }],
+      searchData: {},
+      // 新增
+      title: '审核设置',
+      handelCheck: false
+    }
+  },
+  watch:{
+    '$route'(val){
+      console.log(val)
+      this.columnList()
+      this.columnSearchList()
+    }
   },
   mounted() {
-    let data = {
-      a: 1,
-      b: 2
+    this.columnList()
+  },
+  created() {
+    this.columnSearchList()
+  },
+  methods: {
+    // 返回
+    goBack() {
+
+    },
+    addCheck() {
+      this.handelCheck = true
+    },
+    editCheck() {
+     this.handelCheck = true
+    },
+    columnSearchList() {
+      var _this = this
+      return new Promise((resolve, reject) => {
+        columnList({}, 1, 1000)
+          .then((response) => {
+            this.$nextTick(() => {
+              _this.searchSettings[0].options = _this.toTree(response.data.result.content)
+            })
+            resolve()
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
+    },
+    channelNameChange(val) {
+      let arr = []
+      if(val) {
+        arr = val.split(',').concat([''])
+      } else {
+        arr = []
+      }
+      return arr
+    },
+    checkAuth (authKey) {
+      if (this.$store.getters.authorities.indexOf(authKey) === -1) {
+        return false
+      } else {
+        return true
+      }
+    },
+    searchItem(searchData) {
+      this.searchData = searchData
+      if(this.searchData.channelName && this.searchData.channelName.length) {
+        this.searchData.channelName = this.searchData.channelName[this.searchData.channelName.length - 1]
+      } else {
+        this.searchData.channelName = ''
+      }
+      this.pageNum = 1
+      this.columnList()
+    },
+    columnList() {
+      var _this = this
+      return new Promise((resolve, reject) => {
+        columnList(_this.searchData, _this.pageNum, _this.pageSize)
+          .then((response) => {
+            _this.tableData = response.data.result.content
+            _this.totalCount = response.data.result.total
+            resolve()
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
+    },
+    columnDelFeatch(row) {
+      var _this = this
+      return new Promise((resolve, reject) => {
+        deleteColumn({ channelId: row.channelId })
+          .then((response) => {
+            _this.columnList()
+            console.log(321)
+            if (response.data.code === 0) {
+              _this.$message.success('操作成功！')
+            } else {
+              _this.$message.success(response.data.msg)
+            }
+            resolve()
+          })
+          .catch((error) => {
+            reject(error)
+          })
+      })
+    },
+    handleSizeChange(val) {
+      this.pageSize = val
+      this.columnList()
+    },
+    handleCurrentChange(val) {
+      this.pageNum = val
+      this.columnList()
+    },
+    columnAddEdit(handelType, type, channelId) {
+      this.$router.push({
+        path: '/cms/website/columnHandel',
+        query: {
+          isAdd: handelType,
+          isFather: type === 'father',
+          channelId: channelId
+        }
+      })
+    },
+    columnTemplate(row) {
+      this.$router.push({
+        path: '/cms/website/columnTemplate',
+        query: {
+          channelId: row.channelId,
+          parentChannelId: row.parentChannelId
+        }
+      })
+    },
+    extendsWord(row) {
+      this.$router.push({
+        path: '/cms/website/extendsWord',
+        query: {
+          channelId: row.channelId
+        }
+      })
+    },
+    tagSetting(row) {
+      this.$router.push({
+        path: '/cms/website/tagSetting',
+        query: {
+          channelId: row.channelId
+        }
+      })
+    },
+    waterSetting(row) {
+      this.$router.push({
+        path: '/cms/website/waterSetting',
+        query: {
+          channelId: row.channelId
+        }
+      })
+    },
+    columnDel(row) {
+      this.$confirm('此操作将永久删除该栏目, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.columnDelFeatch(row)
+      }).catch(() => {
+      })
     }
-    console.log(1222)
-    console.log(qs.stringify(data), 'formdata')
   }
 }
 </script>
+
+<style lang='scss'>
+.check-setting {
+   margin: 30px;
+  .tool-bar {
+    margin-top:22px;
+  }
+}
+</style>
