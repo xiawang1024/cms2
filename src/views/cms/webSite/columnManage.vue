fd320e042d1c692d3bddfb604e50055656d3e99b
zhaojinpeng <1406737142@qq.com>
2019-05-17 17:49:40 +0800

    细节优化

--- src/views/cms/webSite/columnManage.vue
+++ src/views/cms/webSite/columnManage.vue
@@ -41,8 +41,8 @@
       </el-table-column>
       <el-table-column label="操作" width="150">
         <template slot-scope="scope">
-          <el-button type="text" v-if="checkAuth('cms:channel:delete')" @click="columnAddEdit(false, '', scope.row.channelId)">编辑</el-button>
-          <el-button type="text" v-if="checkAuth('cms:channel:edit')" @click="columnDel(scope.row)" >删除</el-button>
+          <el-button type="text" v-if="checkAuth('cms:channel:edit')" @click="columnAddEdit(false, '', scope.row.channelId)">编辑</el-button>
+          <el-button type="text" v-if="checkAuth('cms:channel:delete')" @click="columnDel(scope.row)" >删除</el-button>
           <el-button type="text" @click="columnAddEdit(true, 'child', scope.row.channelId)" v-if="checkAuth('cms:channel:add')">添加</el-button>
         </template>
       </el-table-column>
