99b2cecfafb0e205dc71e325394db1ac1a6524b1
liukai <1587680247@qq.com>
2019-07-02 17:56:45 +0800

    优化

--- src/views/live/liveConfig/streamAdressManage.vue
+++ src/views/live/liveConfig/streamAdressManage.vue
@@ -4,11 +4,16 @@
       <el-tabs v-model="activeName" @tab-click="handleClick">
         <el-tab-pane label="鉴权直播" name="0"/>
         <el-tab-pane label="拉转直播" name="1"/>
-        <el-tab-pane label="+直播快照" name="2"/>
       </el-tabs>
     </template>
     <el-table :data="tableValue">
       <el-table-column type="index" width="50" />
+      <el-table-column prop="userName" width="180" label="直播快照" >
+        <template slot-scope="scope">
+          <img :src="scope.row.picPath" alt="" class="imgStyle">
+          <!-- <img src="http://172.20.5.4:8080/dl/2019/7/2/a_1.jpg" class="imgStyle" alt=""> -->
+        </template>
+      </el-table-column>
       <el-table-column prop="userName" width="120" label="用户名"/>
       <el-table-column prop="recordBeginTime" width="180" :formatter="formatStart" label="录制开始时间"/>
       <el-table-column prop="pullStream" label="直播流地址"/>
@@ -112,9 +117,6 @@ export default {
       if(this.activeName=='1'){
         this.tableValue=[]
       }
-      if(this.activeName=='2'){
-        this.tableValue=[]
-      }
 
       },
   }
@@ -126,5 +128,11 @@ export default {
 }
 .pagenation{
   margin: 30px 0;
+}
+.imgStyle{
+  display: block;
+  width: 160px;
+  margin: 5px 0;
+  border-radius: 5px;
 }
 </style>
