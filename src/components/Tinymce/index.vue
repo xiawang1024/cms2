ff0b445c23d3068729d7e4c0f2370c3de87c736f
zhaojinpeng <1406737142@qq.com>
2019-07-05 14:50:00 +0800

    图片缩放

--- src/components/Tinymce/index.vue
+++ src/components/Tinymce/index.vue
@@ -114,7 +114,8 @@ export default {
         selector: `#${this.tinymceId}`,
         height: this.height,
         body_class: 'panel-body ',
-        object_resizing: false,
+        // 是否可以缩放图片
+        // object_resizing: false,
         toolbar: this.toolbar.length > 0 ? this.toolbar : toolbar,
         // toolbar: 'imageupload',
         menubar: this.menubar,
@@ -156,67 +157,8 @@ export default {
             _this.fullscreen = e.state
           })
         },
-        // link_list: [
-        //       { title: '预置链接1', value: 'http://www.tinymce.com' },
-        //       { title: '预置链接2', value: 'http://tinymce.ax-z.cn' }
-        //   ],
-        //   image_list: [
-        //       { title: '预置图片1', value: 'https://www.tiny.cloud/images/glyph-tinymce@2x.png' },
-        //       { title: '预置图片2', value: 'https://www.baidu.com/img/bd_logo1.png' }
-        //   ],
-        //   image_class_list: [
-        //   { title: 'None', value: '' },
-        //   { title: 'Some class', value: 'class-name' }
-        //   ],
-        // importcss_append: true,
         file_picker_types: 'file image media',
-        // images_upload_handler: function (blobInfo, success, failure) {
-        //     function uploadPic () {
-        //       const xhr = new XMLHttpRequest()
-        //       const formData = new FormData()
-  
-        //       xhr.withCredentials = false
-        //       xhr.open('POST', 'http://fupload.test.dianzhenkeji.com:55030/basefile/upload?fileRefId=')
-        //       xhr.onload = function () {
-        //         var json
-  
-        //         if (xhr.status !== 200) {
-        //           failure('上传失败: ' + xhr.status)
-        //           return
-        //         }
-        //         json = JSON.parse(xhr.responseText)
-        //         if (!json || json.status === 'error') {
-        //           failure('上传失败: ' + xhr.responseText.msg)
-        //           return
-        //         }
-        //         console.log(json, 'location')
-        //         success(json.location)
-        //       }
-        //       formData.append('file', blobInfo.blob())
-        //       console.log(formData, 'formData')
-        //       xhr.send(formData)
-        //     }
-  
-        //     if (blobInfo.blob().size > 1024 * 1024 * 5) {
-        //       failure('文件体积过大, 请选择5MB以下图片文件')
-        //     }
-        //     if (blobInfo.blob().type === 'image/jpeg' || blobInfo.blob().type === 'image/png') {
-        //       uploadPic()
-        //     } else {
-        //       failure('请选择jpeg或png格式图片')
-        //     }
-        //   },
-        // file_picker_callback: function (callback, value, meta) {
-        //     if (meta.filetype === 'file') {
-        //       callback('https://www.baidu.com/img/bd_logo1.png', { text: 'My text' });
-        //     }
-        //     if (meta.filetype === 'image') {
-        //       callback('https://www.baidu.com/img/bd_logo1.png', { alt: 'My alt text' });
-        //     }
-        //     if (meta.filetype === 'media') {
-        //       callback('http://cmsres.test.dianzhenkeji.com/anonymous/2019/4/30/1123120181959331840.mp4', { source2: 'alt.ogg', poster: 'https://www.baidu.com/img/bd_logo1.png' });
-        //     }
-        // },
+
         image_caption: true,
         // //上传文件
         file_picker_callback: function(callback, value, meta) {
@@ -265,41 +207,6 @@ export default {
           }
           input.click()
         }
-        // 整合七牛上传
-        // images_dataimg_filter(img) {
-        //   setTimeout(() => {
-        //     const $image = $(img);
-        //     $image.removeAttr('width');
-        //     $image.removeAttr('height');
-        //     if ($image[0].height && $image[0].width) {
-        //       $image.attr('data-wscntype', 'image');
-        //       $image.attr('data-wscnh', $image[0].height);
-        //       $image.attr('data-wscnw', $image[0].width);
-        //       $image.addClass('wscnph');
-        //     }
-        //   }, 0);
-        //   return img
-        // },
-        // images_upload_handler(blobInfo, success, failure, progress) {
-        //   progress(0);
-        //   const token = _this.$store.getters.token;
-        //   getToken(token)
-        //     .then(response => {
-        //       const url = response.data.qiniu_url;
-        //       const formData = new FormData();
-        //       formData.append("token", response.data.qiniu_token);
-        //       formData.append("key", response.data.qiniu_key);
-        //       formData.append("file", blobInfo.blob(), url);
-        //       upload(formData).then(() => {
-        //         success(url);
-        //         progress(100);
-        //       });
-        //     })
-        //     .catch(err => {
-        //       failure("出现未知问题，刷新页面，或者联系程序员");
-        //       console.log(err);
-        //     });
-        // }
       })
     },
     destroyTinymce() {
@@ -319,8 +226,6 @@ export default {
       window.tinymce.get(this.tinymceId).getContent()
     },
     imageSuccessCBK(arr) {
-      console.log(arr, 'arr')
-      // .insertContent(`<video class="" src="${v.url}" controls="controls"> </video>`)
       const _this = this
       arr.forEach(v => {
         if(v.type.split('/')[0] == 'video') {
@@ -350,31 +255,3 @@ export default {
 }
 </script>
 
-<style scoped>
-.tinymce-container {
-  position: relative;
-  line-height: normal;
-  padding-right: 5px;
-}
-.tinymce-container >>> .mce-fullscreen {
-  z-index: 10000;
-}
-.tinymce-textarea {
-  visibility: hidden;
-  z-index: -1;
-}
-.editor-custom-btn-container {
-  position: absolute;
-  right: 4px;
-  top: 4px;
-  /*z-index: 2005;*/
-}
-.fullscreen .editor-custom-btn-container {
-  z-index: 10000;
-  position: fixed;
-}
-.editor-upload-btn {
-  display: inline-block;
-  margin-right: 10px;
-}
-</style>
