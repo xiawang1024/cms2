6360c0d708c983ff0239e1d34587d9e5c3f26657
liukai <you@example.com>
2019-11-04 10:44:43 +0800

    屏蔽老编辑组件

--- src/views/live/liveConfig/streamFileManage.vue
+++ src/views/live/liveConfig/streamFileManage.vue
@@ -47,8 +47,8 @@
         <el-table-column prop="filePath" label="文件路径" show-overflow-tooltip />
         <el-table-column label="操作" width="180">
           <template slot-scope="scope">
-            <el-button size="mini" type="primary" :disabled="scope.row.filePath==null||scope.row.filePath==''" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-            <el-button size="mini" type="primary" :disabled="scope.row.filePath==null||scope.row.filePath==''" @click="handleDeal(scope.$index, scope.row)">处理</el-button>
+            <!-- <el-button size="mini" type="primary" :disabled="scope.row.filePath==null||scope.row.filePath==''" @click="handleEdit(scope.$index, scope.row)">编辑</el-button> -->
+            <el-button size="mini" type="primary" :disabled="scope.row.filePath==null||scope.row.filePath==''" @click="handleDeal(scope.$index, scope.row)">编辑</el-button>
             <el-button
               size="mini"
               type="danger"
