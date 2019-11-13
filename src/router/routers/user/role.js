8166d0bb38a60f9b38c870e0e94956d129d3ba5f
zhaojinpeng <1406737142@qq.com>
2019-11-13 10:05:36 +0800

    权限管理

--- src/router/routers/user/role.js
+++ src/router/routers/user/role.js
@@ -1,6 +1,8 @@
 import Layout from '@/views/layout/Layout'
 
-import { userPath } from '../config'
+import {
+  userPath
+} from '../config'
 const path = `views/${userPath}/role`
 
 console.log(path)
@@ -8,16 +10,24 @@ console.log(path)
 const role = {
   path: `/${userPath}/role`,
   component: Layout,
-  meta: { title: 'role', icon: 'component', permission: 'admin' },
-  children: [
-    {
-      path: '',
-      component: () => import(`@/${path}/index`),
-      name: 'role',
-      meta: { title: 'role', icon: 'component', permission: 'admin' },
-      hidden: true
-    }
-  ]
+  meta: {
+    title: 'role',
+    icon: 'component',
+    permission: 'admin',
+    role: "userCenter:role"
+  },
+  children: [{
+    path: '',
+    component: () => import(`@/${path}/index`),
+    name: 'role',
+    meta: {
+      title: 'role',
+      icon: 'component',
+      permission: 'admin',
+      role: "userCenter:role"
+    },
+    hidden: true
+  }]
 }
 
 export default role
