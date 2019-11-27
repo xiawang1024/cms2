93d4827f6720f442ad873232c7a87a673938fd9f
liukai <you@example.com>
2019-11-27 18:24:01 +0800

    接口调试

--- src/api/user/userInfo.js
+++ src/api/user/userInfo.js
@@ -9,9 +9,29 @@ import request from '@/utils/request'
  */
 export function userInfoList(data) {
   return request({
-    url: ``,
-    method: 'get'
+    url: `/userc/user/queryUserInfor?pageNo=${data.pageNo}&pageSize=${data.pageSize}&nickname=${data.nickname}&mobile=${data.mobile}`,
+    method: 'post'
   })
 }
 
+/**
+ *  query 分组数据查询
+ */
+export function groupingList() {
+  return request({
+    url: `/userc/org/queryOrg`,
+    method: 'post'
+  })
+}
+
+/**
+ *  query 批量更新用户组织
+ */
+export function batchUserGroup(data) {
+  return request({
+    url: `/userc/user/batchUpdateOrg`,
+    method: 'post',
+    data
+  })
+}
 
--- src/views/user/userInfo/index.vue
+++ src/views/user/userInfo/index.vue
@@ -11,31 +11,50 @@
           />
         </el-col>
       </el-row>
+      <el-row>
+        <el-button
+          @click="handleGroupManage"
+          size="mini"
+          type="primary"
+          :disabled="!selectUser.length>0"
+        >批量操作</el-button>
+      </el-row>
     </div>
-    <el-table :data="tableData">
-      <!-- 图像  用户名  昵称  邮箱  性别  手机  邀请码  分组  0：个人 1：机构  0：没直播权限 1：有直播权限  0：没认证 1:认证  赞扬数  最喜欢数  粉丝数  来源  创建时间    首次登录时间  openid  unionid    生日  国家城市   -->
-      <el-table-column prop="description" label="图像">
+    <el-table :data="tableData" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" />
+      <el-table-column prop="icon" label="图像">
         <template slot-scope="scope">
-          <img :src="scope.row.url" alt style="width:60px" >
+          <img :src="scope.row.icon" alt style="width:60px" >
         </template>
       </el-table-column>
-      <el-table-column prop="description" label="用户名" />
-      <el-table-column prop="description" label="昵称" />
-      <el-table-column prop="description" label="邮箱" />
-      <el-table-column prop="description" label="性别" />
-      <el-table-column prop="description" label="手机" />
-      <el-table-column prop="description" label="邀请码" />
+      <el-table-column prop="description" label="用户名" min-width="150px" />
+      <el-table-column prop="nickname" label="昵称" min-width="180px" />
+      <el-table-column prop="email" label="邮箱" min-width="160px" />
+      <el-table-column prop="gender" label="性别">
+        <template slot-scope="scope">
+          <span v-if="scope.row.gender==1">男</span>
+          <span v-if="scope.row.gender==0">女</span>
+        </template>
+      </el-table-column>
+      <el-table-column prop="mobile" label="手机" min-width="120px" />
+      <el-table-column prop="inviteCode" label="邀请码" />
       <el-table-column prop="description" label="分组" />
-      <el-table-column prop="description" label="赞扬数" />
-      <el-table-column prop="description" label="最喜欢数" />
-      <el-table-column prop="description" label="粉丝数" />
-      <el-table-column prop="description" label="来源" />
-      <el-table-column prop="description" label="创建时间" min-width="120px" />
-      <el-table-column prop="description" label="首次登录时间" min-width="120px" />
-      <el-table-column prop="description" label="openid" />
-      <el-table-column prop="description" label="unionid" />
-      <el-table-column prop="description" label="生日" />
-      <el-table-column prop="description" label="国家城市" />
+      <el-table-column prop="fansCount" label="赞扬数" />
+      <el-table-column prop="favoriteCount" label="最喜欢数" />
+      <el-table-column prop="fansCount" label="粉丝数" />
+      <el-table-column prop="origin" label="来源" min-width="160px" />
+      <el-table-column prop="createtime" label="创建时间" min-width="160px" :formatter="timeHandle" />
+      <el-table-column
+        prop="firstLogonTime"
+        label="首次登录时间"
+        min-width="160px"
+        :formatter="timeHandle"
+      />
+      <el-table-column prop="openid" label="openid" min-width="250px" />
+      <el-table-column prop="unionid" label="unionid" min-width="300px" />
+      <el-table-column prop="birthday" label="生日" min-width="120px" />
+      <el-table-column prop="country" label="国家" />
+      <el-table-column prop="city" label="城市" />
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button size="mini" type="primary" @click="handleEdite(scope.$index, scope.row)">编辑</el-button>
@@ -54,99 +73,190 @@
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
     />
+    <el-dialog :visible.sync="dialogVisible" title="分组" width="820px;">
+      <v-form
+        ref="vform"
+        :form-settings="formSettings"
+        :form-data="formData"
+        @save="submitSave"
+        label-width="130px"
+      />
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import { userInfoList } from "@/api/user/userInfo.js";
-
+import {
+  userInfoList,
+  groupingList,
+  batchUserGroup
+} from "@/api/user/userInfo.js";
+import dayjs from "dayjs";
 export default {
   data() {
     return {
       pageNo: 1,
-      pageSize: 30,
+      pageSize: 15,
+      mobile: "",
+      nickname: "",
       total: 0,
       tableData: [],
+      selectUser: [],
+      dialogVisible: false,
       searchSettings: [
         {
-          label: "来源",
-          name: "origin",
-          type: "select",
+          label: "昵称",
+          name: "nickname",
+          type: "text",
           visible: "true",
-          value: "",
-          changeOnSelect: true,
-          filterable: true,
-          options: []
+          value: ""
         },
         {
-          label: "最近",
-          name: "quckTime",
-          type: "select",
-          visible: true,
-          value: "",
-          options: [
-            { label: "7天", value: "7" },
+          label: "手机号",
+          name: "mobile",
+          type: "text",
+          visible: "true",
+          value: ""
+        }
+      ],
+      formData: {},
+      formSettings: [
+        {
+          items: [
             {
-              label: "30天",
-              value: "30"
+              label: "组别",
+              type: "select",
+              name: "orgId",
+              options: []
             }
           ]
-        },
-        {
-          label: "开始时间",
-          name: "beginTime",
-          visible: true,
-          type: "datetime",
-          defaultTime: "00:00:00",
-          value: ""
-        },
-        {
-          label: "结束时间",
-          name: "endTime",
-          visible: true,
-          type: "datetime",
-          defaultTime: "23:59:59",
-          value: ""
         }
       ]
     };
   },
   created() {
     this.init();
+    this.getGroup();
   },
   methods: {
-    init() {},
-    searchItem(val){
-        console.log(val,'val')
-
+    init() {
+      return new Promise((resolve, reject) => {
+        let data = {
+          pageNo: this.pageNo,
+          pageSize: this.pageSize,
+          nickname: this.nickname,
+          mobile: this.mobile
+        };
+        userInfoList(data)
+          .then(res => {
+            if (res.data.code == 0) {
+              this.tableData = res.data.result.content;
+              this.total = res.data.result.total;
+            } else {
+              this.$message.error(res.data.msg);
+            }
+            resolve();
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
     },
-    reset(){
+    getGroup() {
+      return new Promise((resolve, reject) => {
+        groupingList()
+          .then(res => {
+            if (res.data.code == 0) {
+              let data = res.data.result;
 
+              data.forEach((item, index) => {
+                item.label = item.groupName;
+                item.value = item.id;
+              });
+              this.formSettings[0].items[0].options = data;
+              console.log(res.data.result, "aaa");
+            } else {
+              this.$message.error(res.data.msg);
+            }
+            resolve();
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
     },
-
+    timeHandle(val) {
+      let time = "";
+      if (val.createtime) {
+        return (time = dayjs(val.createtime).format("YYYY-MM-DD HH:mm:ss"));
+      }
+    },
+    searchItem(val) {
+      this.pageNo = 1;
+      this.pageSize = 15;
+      this.total = 0;
+      this.tableData = [];
+      console.log(val, "val");
+      if (val.nickname) {
+        this.nickname = val.nickname;
+      }
+      if (val.mobile) {
+        this.mobile = val.mobile;
+      }
+      this.init();
+    },
+    reset() {
+      this.nickname = "";
+      this.mobile = "";
+    },
+    handleSelectionChange(val) {
+      console.log(val, "val");
+      this.selectUser = [];
+      val.forEach((item, index) => {
+        this.selectUser.push(item.id);
+      });
+    },
+    handleGroupManage() {
+      this.dialogVisible = true;
+    },
+    submitSave(val) {
+      //保存分组
+      let data = {
+        orgId: val.orgId,
+        userIdList: this.selectUser
+      };
+      return new Promise((resolve, reject) => {
+        batchUserGroup(data)
+          .then(res => {
+            if (res.data.code == 0) {
+              this.$message.success(res.data.msg);
+              this.dialogVisible = false;
+              this.init();
+            } else {
+              this.$message.error(res.data.msg);
+            }
+            resolve();
+          })
+          .catch(err => {
+            reject(err);
+          });
+      });
+    },
     //分页处理
     handleSizeChange(val) {
       this.pageSize = val;
-      if (this.pageFlag) {
-        this.getList();
-      } else {
-        this.SearchAttribute();
-      }
+      this.init();
     },
     handleCurrentChange(val) {
-      this.pageNum = val;
-      if (this.pageFlag) {
-        this.getList();
-      } else {
-        this.SearchAttribute();
-      }
+      this.pageNo = val;
+      this.init();
     }
   }
 };
 </script>
 
 <style lang='scss' scoped>
-.fenyeDiv{
-    margin-top: 30px;
+.fenyeDiv {
+  margin-top: 30px;
 }
 </style>
