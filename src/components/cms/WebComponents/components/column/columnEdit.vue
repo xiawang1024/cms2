f4838fe068f893db150ab32d38ea7a873c3bdb89
liyu137218 <731234740@qq.com>
2019-12-11 14:29:38 +0800

    发布里的模板权限修正

--- src/components/cms/WebComponents/components/column/columnEdit.vue
+++ src/components/cms/WebComponents/components/column/columnEdit.vue
@@ -4,13 +4,13 @@
       <el-tab-pane label="栏目基本信息" name="information">
         <BasicInformation :active-name = "activeName" :channel-id = "treeTags[treeTags.length - 1].id"/>
       </el-tab-pane>
-      <el-tab-pane label="设置栏目模板" name="templet" v-if="contextMenu.label !== '建立子栏目'">
+      <el-tab-pane label="设置栏目模板" name="templet" v-if="contextMenu.label !== '建立子栏目' && checkAuth('cms:channel:template')">
         <ColumnTemplet :active-name = "activeName" :channel-array = "treeTags"/>
       </el-tab-pane>
-      <el-tab-pane label="拓展字段" name="field" v-if="contextMenu.label !== '建立子栏目'">
+      <el-tab-pane label="拓展字段" name="field" v-if="contextMenu.label !== '建立子栏目' && checkAuth('cms:channel:extendsWord')">
         <ExtendField :active-name = "activeName" :channel-id = "treeTags[treeTags.length - 1].id"/>
       </el-tab-pane>
-      <el-tab-pane label="标签设置" name="label" v-if="contextMenu.label !== '建立子栏目'">
+      <el-tab-pane label="标签设置" name="label" v-if="contextMenu.label !== '建立子栏目' && checkAuth('cms:channel:tagSetting')">
         <LabelSetting :active-name = "activeName" :channel-id = "treeTags[treeTags.length - 1].id"/>
       </el-tab-pane>
     </el-tabs>
@@ -35,9 +35,17 @@ export default {
     ...mapGetters(['treeTags', 'contextMenu'])
   },
   mounted() {
+    
   },
   methods: {
-    handleClick() {}
+    handleClick() {},
+        checkAuth (authKey) {
+      if (this.$store.getters.authorities.indexOf(authKey) === -1) {
+        return false
+      } else {
+        return true
+      }
+    },
   }
 }
 </script>
