7cb20b8a7af3e74a79584bce241df7b8c2a51aa7
liyu137218 <731234740@qq.com>
2019-12-23 16:26:32 +0800

    编辑器的栏目展示 和大小调整

--- src/components/Tinymce/toolbar.js
+++ src/components/Tinymce/toolbar.js
@@ -1,5 +1,5 @@
 // Here is a list of the toolbar
 // Detail list see https://www.tinymce.com/docs/advanced/editor-control-identifiers/#toolbarcontrols
 //| template  restoredraft 
-const toolbar = [' undo redo |  bold  |fontselect | fontsizeselect   | alignleft aligncenter alignright   | movehang indent2em | removeformat    |  code | hr | blockquote| bullist numlist | link  pagebreak  | axupimgs  media  table   | preview fullscreen |']
+const toolbar = [' undo redo |  bold  |fontselect | fontsizeselect   | alignleft aligncenter alignright   | movehang indent2em | removeformat      code  hr  blockquote| bullist numlist link  pagebreak  | axupimgs  media  table   | preview fullscreen |']
 export default toolbar
